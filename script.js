641cc95d2fa9884252e3be8353c3c052d3050af2
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2024-09-05 00:15:37 -0600

    Add new interactivity with JS to validate my form inputs

--- script.js
+++ script.js
@@ -23,28 +23,48 @@ class DOMElements {
 
 const singUpElements = new DOMElements ('first-name','last-name','username','e-mail','password','submit-btn');*/
 
+
+
 const form = document.getElementById('my-form');
 const btn = document.getElementById('submit-btn');
+let labels = document.querySelectorAll('label');
+const nameInput = document.getElementById('first-name');
 
+nameInput.addEventListener('input',()=>{
+  let name = document.getElementById('first-name').value.trim();
+  let namePattern = /^[A-Za-z]+$/;
+  
 
+  if(name === '' || !namePattern.test(name)){
+    labels[0].innerText = 'Enter a valid name';
+    labels[0].style.color = 'red';
+    } 
+    else{
+      labels[0].innerText = 'Valid name, move to the next input.';
+      labels[0].style.color = 'green';
+    }
+})
 
+/*
 form.addEventListener('submit', (event)=>{
 
   event.preventDefault();
 
-  let name = document.getElementById('first-name').value;
-  console.log(name);
-
-  if (name === '' ){
-    console.log('Please add a valid name');
-  } else {
-    console.log('This is a valid name');
-  }
+  let name = document.getElementById('first-name').value.trim();
+  let lastName = document.getElementById('last-name').value.trim();
+  let namePattern = /^[A-Za-z]+$/;
 
 
-
-
-})
+   if(namePattern === ''){
+    console.log('Do not input blanks , name has to be a valid name')
+    }
+     else if (!namePattern.test(name)){
+      console.log('Enter a valid name')
+    } 
+    else{
+      console.log('Your name is valid , thank you!')
+    }
+})*/
 
 
 
