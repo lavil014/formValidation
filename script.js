39a39b8c6ec1e96d079acaf1abd4fd73b20f9e07
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2024-09-21 21:57:10 -0600

    New changes to my script file

--- script.js
+++ script.js
@@ -168,52 +168,7 @@ form.addEventListener('submit',(e)=>{
   } 
 })
 
-/*
 
-class DOMElements {
-    constructor(name, lastName, userName,eMail,passWord, btn, ){
-      this._name = document.getElementById(name);
-      this._lastName = document.getElementById(lastName);
-      this._userName = document.getElementById(userName);
-      this._eMail = document.getElementById(eMail);
-      this._passWord = document.getElementById(passWord);
-      this._btn = document.getElementById(btn);
-    }
-
-    validateName (event){
-      event.preventDefault();
-
-      let name = this._name.value;
-      this._btn.addEventListener('click',()=>{
-        console.log("Event listener is working");
-      })
-      
-    }
-}
-
-const singUpElements = new DOMElements ('first-name','last-name','username','e-mail','password','submit-btn');*/
-
-
-/*
-form.addEventListener('submit', (event)=>{
-
-  event.preventDefault();
-
-  let name = document.getElementById('first-name').value.trim();
-  let lastName = document.getElementById('last-name').value.trim();
-  let namePattern = /^[A-Za-z]+$/;
-
-
-   if(namePattern === ''){
-    console.log('Do not input blanks , name has to be a valid name')
-    }
-     else if (!namePattern.test(name)){
-      console.log('Enter a valid name')
-    } 
-    else{
-      console.log('Your name is valid , thank you!')
-    }
-})*/
 
 
 
