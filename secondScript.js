484144ebf544961f8f3b88a3743aa0a2f7790ca0
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2024-09-22 17:03:59 -0600

    Updte script files

--- secondScript.js
+++ secondScript.js
@@ -4,29 +4,31 @@ let labels = document.querySelectorAll('label');
 const inputs = document.querySelectorAll('input');
 let defaultLabelTexts = Array.from(labels).map(label => label.innerText);
 
+
+/* Object to validtion rules in inputs */
 const validationRules = {
     'firstNme' :{
-      patern: /^[A-Za-z]+$/,
+      pattern: /^[A-Za-z]+$/,
       successMessage : 'Valid name, move to the next input.',
       failureMessage: 'Enter a valid name',
     },
     'lstnme': {
-      patern: /^[A-Za-z]+$/,
+      pattern: /^[A-Za-z]+$/,
       successMessage : 'Valid name, move to the next input.',
       failureMessage: 'Enter a valid name',
     },
     'userName':{
-      patern: /^[A-Za-z]+$/,
+      pattern: /^[A-Za-z]+$/,
       successMessage : 'Valid user name.',
       failureMessage: 'Username must be between 8 and 16 characters',
     },
     'emil':{
-      patern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
       successMessage : 'Valid email address.',
       failureMessage: 'Enter a valid email address',
     },
     'pssword':{
-      patern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
       successMessage : 'Valid email address.',
       emptyPsswordMessge: 'Password cannot be empty',
       failureMessage: '8 characters long,uppercase, lowercase,a number, and a special character',
@@ -48,8 +50,16 @@ const updateUI = (index, message, isValid)=>{
 /* Function to validate inputs*/
 
 const validateInputs = (fieldName,index)=>{
-  let value = inputs[index].value.trim();
-  let rule = validationRules[fieldName];
+  const value = inputs[index].value.trim();
+  const rule = validationRules[fieldName];
+
+  const isValid = rule.pattern ? rule.pattern.test(value) : rule.validate(value);
+
+  if(!isValid){
+    updateUI(index,rule.failureMessage,false);
+  }else{
+    updateUI(index,rule.successMessage, true);
+  }
 
 }
 
@@ -59,3 +69,15 @@ inputs[1].addEventListener('input', ()=>validateInputs('lstNme',1))
 inputs[2].addEventListener('input', ()=>validateInputs('userName',2))
 inputs[3].addEventListener('input', ()=>validateInputs('emil',3))
 inputs[4].addEventListener('input', ()=>validateInputs('pssword',4))
+
+/*Function to validate form information is correct*/
+
+form.addEventListener('submit',(e)=>{
+  let validForm = Array.from(inputs).every(input => input.classList.contains('success'));
+
+  if(!validForm){
+    e.preventDefault();
+    btn.innerText = 'Information is invalid';
+    btn.style.backgroundColor = '#dc3545'; 
+  } 
+})
